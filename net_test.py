d3d95feda3503085ec3d33e9b907cbcf0ab1a731
HDB <huangdunbo@126.com>
2018-09-30 14:25:19 +0800

    Inference test.

--- net_test.py
+++ net_test.py
@@ -10,18 +10,20 @@ from nets import yolo_net
 from preprocessing import yolo_preprocessing
 from datasets import pascalvoc_2012 
 
-image, shape, box, label = pascalvoc_2012.inputs("../dataset/VOCdevkit/pascal_voc21/", "", "Train", 16, None)
-bimage, blabel, bbox = yolo_preprocessing.preprocess_for_train(image, label, box, (448,448))
-
+# 1. Create yolo object
 yolo_base = base_net.YOLO_Base()
 yolo_obj = yolo_net.YOLO()
 
+# 2. Get Dataset and Preprocess + Shuffle Batch
+image, shape, box, label = pascalvoc_2012.inputs("../dataset/VOCdevkit/pascal_voc21/", "", "Train", 16, None)
+bimage, blabel, bbox = yolo_preprocessing.preprocess_for_train(image, label, box, (448,448))
 blabels, bboxes, bscores = yolo_obj.encode_boxes(bbox, blabel)
-
 imagess, boxess, labelss, scoress = tf.train.shuffle_batch([bimage,bboxes,blabels,bscores], batch_size=3, num_threads=64, capacity=5000, min_after_dequeue=3000)
 
-# net = yolo_obj.yolo_net(img, 21)
-# output = yolo_obj.net_loss(net, None, 16)
+# 3. Inference
+y = yolo_obj.yolo_net(imagess, 21)
+
+# loss = yolo_obj.net_loss(y, None, 16)
 
 with tf.Session() as sess:
     coord = tf.train.Coordinator()
@@ -29,10 +31,8 @@ with tf.Session() as sess:
 
     init_op = tf.global_variables_initializer()
     sess.run(init_op)
-    out1, out2, out3, out4 = sess.run([imagess, boxess, labelss, scoress])
-    print out2
-    print out3
-    print out4
+    out1 = sess.run([y])
+    print out1
 
     coord.request_stop()
     coord.join(threads)

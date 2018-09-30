2fc0ff0ae38998598bfda4a5da83f3c962ad84f4
HDB <huangdunbo@126.com>
2018-09-30 14:13:47 +0800

    Edit net_test.py

--- net_test.py
+++ net_test.py
@@ -10,15 +10,11 @@ from nets import yolo_net
 from preprocessing import yolo_preprocessing
 from datasets import pascalvoc_2012 
 
-image, shape, box, label = pascalvoc_2012.inputs("./pascal_voc21/", "", "Train", 16, None)
+image, shape, box, label = pascalvoc_2012.inputs("../dataset/VOCdevkit/pascal_voc21/", "", "Train", 16, None)
 bimage, bbox, blabel = yolo_preprocessing.preprocess_for_train(image, label, box, (448,448))
 
 yolo_base = base_net.YOLO_Base()
 yolo_obj = yolo_net.YOLO()
-
-# img = tf.ones([16,448,448,3])
-# boxes_input = tf.constant([[0.2, 0.2, 0.4, 0.4], [0.4,0.4,0.6,0.6]])
-# labels_input = tf.constant([11, 6], dtype=tf.int64)
 
 blabels, bboxes, bscores = yolo_obj.encode_boxes(bbox, blabel)
 
@@ -27,11 +23,16 @@ imagess, boxess, labelss, scoress = tf.train.shuffle_batch([bimage,bboxes,blabel
 # net = yolo_obj.yolo_net(img, 21)
 # output = yolo_obj.net_loss(net, None, 16)
 
-
 with tf.Session() as sess:
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
     init_op = tf.global_variables_initializer()
     sess.run(init_op)
     out1, out2, out3, out4 = sess.run([imagess, boxess, labelss, scoress])
     print out2
     print out3
     print out4
+
+    coord.request_stop()
+    coord.join(threads)
